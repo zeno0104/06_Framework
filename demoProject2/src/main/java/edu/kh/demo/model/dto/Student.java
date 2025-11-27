986ea35239ad67cb7f3c8de08ff51fa771e52bb1
zeno0104 <jaehun4086@naver.com>
2025-11-27 17:02:41 +0900

    Update Student.java

--- demoProject2/src/main/java/edu/kh/demo/model/dto/Student.java
+++ demoProject2/src/main/java/edu/kh/demo/model/dto/Student.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+// Spring EL 같은 경우 DTO 객체 출력할 때 getter가 필수 작성되어 있어야 함
+// -> ${Student.name} == ${Student.getName()}과 같기 때문
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
